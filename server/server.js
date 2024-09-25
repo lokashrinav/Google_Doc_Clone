42f8ef4b0f54250c11af82ffd0e056995be340c6
Shrinav Loka <lokashrinav@gmail.com>
2024-09-25 16:21:27 -0400

    socket project

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const io = require('socket.io')(3001, {
+    cors: {
+        origin: 'http://localhost:3000', 
+        methods: ['GET', 'POST']
+    }
+})
+
+io.on("connection", socket => {
+    socket.on('send-changes', delta => {
+        socket.broadcast.emit("nd -changes", delta)
+    })
+})
